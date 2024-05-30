ed2c51da46cc915bfea0426a74f9536cfb8f4d09
wx <dominik.sladecek07@gmail.com>
2024-05-30 21:25:22 +0200

    feat: radial menu base

--- client/radialMenu.lua
+++ client/radialMenu.lua
@@ -1,1 +1,20 @@
 if not wx.radialMenus then return end
+
+exports('handcuffHandler', function(menu, item)
+    print(menu, item)
+
+    if menu == 'wx_unijob:radial:police' and item == 1 then
+        print('TODO: HANDCUFFS')
+    end
+end)
+
+lib.registerRadial({
+    id = 'wx_unijob:radial:police',
+    items = {
+        {
+            label = 'Handcuff',
+            icon = 'handcuffs',
+            onSelect = 'myMenuHandler'
+        },
+    }
+})
